a1bda83ae8b48f9a4f87cf7739e1174b61548225
ojhhh <winnel@naver.com>
2024-02-24 18:41:03 +0900

    FEAT: question create (do not use jwt middleware)

--- src/context/context.module.ts
+++ src/context/context.module.ts
@@ -3,8 +3,9 @@ import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AnswerModule } from './answer/answer.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [AccountModule, AuthModule, UsersModule, AnswerModule],
+  imports: [AccountModule, AuthModule, UsersModule, AnswerModule, QuestionModule],
 })
 export class ContextModule {}
---
+++ src/context/question/question.service.ts
@@ -0,0 +1,39 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { PrismaService } from 'src/db/prisma/prisma.service';
+
+@Injectable()
+export class QuestionService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(user_email: string, createQuestionDto: CreateQuestionDto): Promise<{ message: string }>  {
+    try {
+      const { question_target, question_title, question_type } = createQuestionDto;
+    
+      const { user_id } = await this.prismaService.sWYP_User.findUnique({
+        where : {
+          user_email
+        }
+      })
+
+      await this.prismaService.sWYP_Question.create({
+        data: {
+          question_target, 
+          question_title, 
+          question_type,
+          user_id
+        }
+      });
+
+      return { message: 'Your question has been saved successfully.' };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server error.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
+
+
