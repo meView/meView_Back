13325f2baaae3abb31954bf055b90a41f2744913
ojhhh <winnel@naver.com>
2024-02-26 17:40:54 +0900

    REFACTOR: Change Request type in question.controller

--- src/context/question/question.controller.ts
+++ src/context/question/question.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
 import { QuestionService } from './question.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { Request } from 'express';
 
 @Controller('question')
 export class QuestionController {
@@ -13,8 +14,7 @@ export class QuestionController {
     @Req() req: Request,
     @Body() createQuestionDto: CreateQuestionDto
     ) {
-    const user = (req as any).user; 
-    const user_id = user['user_id'];
+    const user_id = req.user['user_id'];
     return this.questionService.create(user_id, createQuestionDto);
   }
 }
