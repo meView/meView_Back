dc63f07145da3eb5320426bd843a307faf12a3d5
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-02-27 16:29:29 +0900

    feat: meview/projects, get all my projects

--- src/context/context.module.ts
+++ src/context/context.module.ts
@@ -3,8 +3,9 @@ import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AnswerModule } from './answer/answer.module';
+import { MeviewModule } from './meview/meview.module';
 
 @Module({
-  imports: [AccountModule, AuthModule, UsersModule, AnswerModule],
+  imports: [AccountModule, AuthModule, UsersModule, AnswerModule, MeviewModule],
 })
 export class ContextModule {}
