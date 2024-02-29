455c18d9a75f2bc8a50053b07153aaed4b891b2a
ojhhh <winnel@naver.com>
2024-02-29 17:57:52 +0900

    feat: Get my question list, Get my question detail function to home.service

---
+++ src/context/home/dto/select-home.dto.ts
@@ -0,0 +1,13 @@
+import { SWYP_QuestionTarget, SWYP_QuestionType } from "@prisma/client";
+
+export class MyQuestionListDto {
+  question_id: number;
+  question_title: string;
+}
+
+export class MyQuestionDetailDto {
+  question_id: number;
+  question_type: SWYP_QuestionType;
+  question_target: SWYP_QuestionTarget;
+  question_title: string;
+}
