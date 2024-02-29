61d4f77642255ad7c2ad84addd08005318b5d3cc
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-02-29 18:53:27 +0900

    feat: home/put method

--- src/context/home/home.controller.ts
+++ src/context/home/home.controller.ts
@@ -6,10 +6,25 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
+  Put,
+  UseInterceptors,
+  UploadedFile,
+  ValidationPipe,
+  HttpException,
+  BadRequestException,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { CreateHomeDto } from './dto/create-home.dto';
 import { UpdateHomeDto } from './dto/update-home.dto';
+import {
+  DeleteQuestionDTO,
+  UpdateQuestionDTO,
+} from './dto/update-homeQuestion.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { Request } from 'express';
 
 @Controller('home')
 export class HomeController {
@@ -22,7 +37,7 @@ export class HomeController {
 
   @Get()
   findAll() {
-    return this.homeService.findAll();
+    return 's';
   }
 
   @Get(':id')
@@ -38,5 +53,27 @@ export class HomeController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.homeService.remove(+id);
+  }
+
+  @Put('question/update/:question_id')
+  @UseGuards(AuthGuard)
+  async updateQuestion(
+    @Req() req: Request,
+    @Param('question_id', ParseIntPipe) question_id: number,
+    @Body(new ValidationPipe()) data: UpdateQuestionDTO,
+  ) {
+    const user_id = req.user['user_id'];
+    return await this.homeService.updateQuestion(user_id, question_id, data);
+  }
+
+  @Put('question/delete/:question_id')
+  @UseGuards(AuthGuard)
+  async updateQuestionIsUsed(
+    @Req() req: Request,
+    @Param('question_id', ParseIntPipe) question_id: number,
+    @Body(new ValidationPipe()) data: DeleteQuestionDTO,
+  ) {
+    const user_id = req.user['user_id'];
+    return await this.homeService.deleteQuestion(user_id, question_id, data);
   }
 }
--- src/context/home/home.service.ts
+++ src/context/home/home.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateHomeDto } from './dto/create-home.dto';
 import { UpdateHomeDto } from './dto/update-home.dto';
+import {
+  DeleteQuestionDTO,
+  UpdateQuestionDTO,
+} from './dto/update-homeQuestion.dto';
+import { PrismaService } from 'src/db/prisma/prisma.service';
 
 @Injectable()
 export class HomeService {
+  constructor(private PrismaService: PrismaService) {}
   create(createHomeDto: CreateHomeDto) {
     return 'This action adds a new home';
   }
@@ -22,5 +28,53 @@ export class HomeService {
 
   remove(id: number) {
     return `This action removes a #${id} home`;
+  }
+
+  async updateQuestion(
+    user_id: number,
+    question_id: number,
+    data: UpdateQuestionDTO,
+  ): Promise<UpdateQuestionDTO> {
+    try {
+      await this.PrismaService.sWYP_Question.findUniqueOrThrow({
+        where: { question_id, user_id },
+      });
+
+      const updatedQuestion = await this.PrismaService.sWYP_Question.update({
+        where: { question_id, user_id },
+        data: {
+          question_target: data.question_target,
+          question_type: data.question_type,
+          question_title: data.question_title,
+        },
+      });
+
+      return updatedQuestion;
+    } catch (error) {
+      throw new HttpException('Bad Request', 400);
+    }
+  }
+
+  async deleteQuestion(
+    user_id: number,
+    question_id: number,
+    data: DeleteQuestionDTO,
+  ): Promise<DeleteQuestionDTO> {
+    try {
+      await this.PrismaService.sWYP_Question.findUniqueOrThrow({
+        where: { question_id, user_id },
+      });
+
+      const deletedQuestion = await this.PrismaService.sWYP_Question.update({
+        where: { question_id, user_id },
+        data: {
+          is_used: data.is_used,
+        },
+      });
+
+      return deletedQuestion;
+    } catch (error) {
+      throw new HttpException('Bad Request', 400);
+    }
   }
 }
